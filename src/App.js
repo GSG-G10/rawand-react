9aafa24a68d2be844cb5cd31e0ed0f8b87abd62b
rawand1998 <rawandgaradh@gmail.com>
2021-09-21 15:50:34 +0300

    navBar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import NavBar from "./Components/NavBar";
 function App(){
     return(
-        <div>hello</div>
+        <NavBar />
+       
     )
 
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+function NavBar(){
+    return(
+        <div>
+            <h1 className="logo">Food</h1>
+            <ul className="nav-bar">
+                <li>
+                 Home
+                </li>
+                <li>
+                 About
+                </li>
+                <li>
+                 Recipe food
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default NavBar;
